b2056dd08b698fd35471565f30298e91c090417b
CODA <114365761+codaDEVV@users.noreply.github.com>
2023-10-16 09:11:06 -0700

    first commit

---
+++ src/main/java/jade/Window.java
@@ -0,0 +1,16 @@
+package jade;
+
+public class Window {
+    private int width,height;
+    private String title;
+
+    private Window(){
+        this.height = 1080;
+        this.width = 1920;
+        this.title = "Mario";
+    }
+
+    public static Window.get(){
+    }
+}
+
